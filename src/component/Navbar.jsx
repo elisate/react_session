c8f888eeb479785878ecebfec4f31baf9440aac8
elisate <dushimiyimanaelisa@gmail.com>
2026-05-20 15:32:42 -0500

    fix some set up

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,6 +4,7 @@ import { IoPersonOutline } from "react-icons/io5";
 import { LiaShoppingBagSolid } from "react-icons/lia";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import {ChevronDown } from "lucide-react";
+import { Link } from 'react-router';
 import "../styles/navbar.css"
 function Navbar() {
   return (
@@ -12,11 +13,11 @@ function Navbar() {
     <span className='logo'>miniture</span>
    </div>
    <ul className='list-menu'>
-    <li><a href="/"  className='l-1'><span>Home</span>&nbsp;<ChevronDown  className='icon-size-1' /></a></li>
-    <li><a href="/about" className='l-2'><span>Catalogue</span>&nbsp;<ChevronDown  className='icon-size-1' /></a></li>
-    <li><a href="/contact" className='l-3'><span>Pages</span>&nbsp;<ChevronDown  className='icon-size-1' /></a></li>
-    <li><a href="/blog">Blog</a></li>
-    <li><a href="/contact">Contact</a></li>
+    <li><Link to="/"  className='l-1'><span>Home</span>&nbsp;<ChevronDown  className='icon-size-1' /></Link></li>
+    <li><Link to="/about" className='l-2'><span>Catalogue</span>&nbsp;<ChevronDown  className='icon-size-1' /></Link></li>
+    <li><Link to="/contact" className='l-3'><span>Pages</span>&nbsp;<ChevronDown  className='icon-size-1' /></Link></li>
+    <li><Link to="/blog">Blog</Link></li>
+    <li><Link to="/contact">Contact</Link></li>
    </ul>
    <div className='icons-Navbar'>
     <IoSearch className='icon-size' />
